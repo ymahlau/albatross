a626cfb3bfe2748d223cbaa9758d2417a0223b4c
ymahlau <mahlauyannik@gmail.com>
2023-11-02 12:02:19 +0100

    maybe fixed automatic unit tests x4

--- src/cpp/alglib/linux_compile.sh
+++ src/cpp/alglib/linux_compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Compilation of Alglib Environment
 # Run with sh linux_compile.sh
+
 echo "Started Compilation of Alglib"
 g++ -w -std=c++11 -c -fPIC -O3 ap.cpp -o ../compiled/ap.o
 g++ -w -std=c++11 -c -fPIC -O3 alglibinternal.cpp -o ../compiled/alglibinternal.o
