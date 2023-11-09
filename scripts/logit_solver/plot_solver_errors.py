0feb307217845400462150bfb6958fd2bd128907
ymahlau <mahlauyannik@gmail.com>
2023-11-09 15:40:03 +0100

    Fully coop plots

--- scripts/logit_solver/plot_solver_errors.py
+++ scripts/logit_solver/plot_solver_errors.py
@@ -56,6 +56,52 @@ def plot_pol_errors():
     plt.savefig(img_path)
     plt.show()
 
+
+def plot_pol_errors_fc():
+    all_iterations = np.arange(50, 2001, 50)
+
+    plt.clf()
+    plt.figure(dpi=400)
+    seaborn.set_theme(style='whitegrid')
+
+    # methods = ['MSA', 'POLYAK', 'NAGURNEY']
+    # methods = ['MSA', 'POLYAK', 'NAGURNEY', 'SRA']
+    methods = ['POLYAK', 'NAGURNEY']
+
+    for m_idx, m in enumerate(methods):
+        data_list = []
+        path = Path(__file__).parent.parent.parent.parent / 'a_data' / 'logit_solver' / f"{m}_fc"
+        all_iterations = np.arange(50, 2001, 50)
+        for cur_iterations in all_iterations:
+            file_name = f'{m}_zs_{cur_iterations}.pkl'
+            with open(path / file_name, 'rb') as f:
+                cur_data = pickle.load(f)
+            data_list.append(cur_data)
+        avg_errors = np.asarray([np.mean(d.errors) for d in data_list])
+        std_errors = np.asarray([np.std(d.errors) for d in data_list])
+
+        plot_filled_std_curves(
+            x=all_iterations,
+            mean=avg_errors,
+            std=std_errors,
+            color=COLORS[m_idx],
+            lighter_color=LIGHT_COLORS[m_idx],
+            linestyle=LINESTYLES[m_idx],
+            label=m,
+            min_val=0,
+        )
+
+    plt.legend()
+    plt.xlabel('Number of Iterations')
+    plt.ylabel('Average Absolute Policy Error')
+    plt.tight_layout()
+    # plt.xlim(all_iterations[0], all_iterations[-1])
+
+    img_path = Path(__file__).parent.parent.parent.parent / 'a_data' / 'logit_solver' / 'a_img' / 'fc_pol.png'
+    plt.savefig(img_path)
+    plt.show()
+
+
 def plot_value_errors():
     all_iterations = np.arange(50, 2001, 50)
     gt_path = Path(__file__).parent.parent.parent.parent / 'a_data' / 'logit_solver' / 'ground_truth_zs_0_10.pkl'
@@ -101,4 +147,5 @@ def plot_value_errors():
 
 if __name__ == '__main__':
     # plot_pol_errors()
-    plot_value_errors()
+    plot_pol_errors_fc()
+    # plot_value_errors()
