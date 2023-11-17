d1161dacae1c8bf5266e7bf0f10e8395093704b9
ymahlau <mahlauyannik@gmail.com>
2023-11-17 14:35:07 +0100

    fixed another test case

--- test/equilibria/test_nash_efficiency.py
+++ test/equilibria/test_nash_efficiency.py
@@ -18,24 +18,12 @@ class TestNashEfficiency(unittest.TestCase):
         joint_action_list = list(itertools.product(*available_actions))
         for _ in range(num_runs):
             joint_action_values = np.random.rand(len(joint_action_list), 2)
-            # python
             start_time = time.time()
-            values_python, policies_python = calculate_nash_equilibrium(
-                available_actions,
-                joint_action_list,
-                joint_action_values,
-                use_cpp=False,
-            )
-            python_time += time.time() - start_time
-            # cpp
-            start_time = time.time()
-            values_cpp, policies_cpp = calculate_nash_equilibrium(
+            values, policies = calculate_nash_equilibrium(
                 available_actions,
                 joint_action_list,
                 joint_action_values,
                 use_cpp=True,
             )
-            cpp_time += time.time() - start_time
-        print(f"\n{python_time=}")
+            python_time += time.time() - start_time
         print(f"{cpp_time=}")
-        self.assertLess(cpp_time, python_time)
